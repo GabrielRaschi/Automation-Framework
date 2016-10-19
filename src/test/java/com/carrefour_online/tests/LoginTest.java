9ddabe8ed4f3125dfac672fc879ec4db83764f2d
GMihai <gmihai@tremend.ro>
2016-10-19 17:29:18 +0300

    Add test result output to excel file

--- src/test/java/com/carrefour_online/tests/LoginTest.java
+++ src/test/java/com/carrefour_online/tests/LoginTest.java
@@ -10,14 +10,15 @@ public class LoginTest extends DriverBase {
 
     @Test
             (groups = "Test")
-    public void VerifyHomepage() throws Exception {
-
+    public void VerifyLandingPage() throws Exception {
         WebDriver driver = getDriver();
 
         Carrefour_Base base = new Carrefour_Base(driver);
-        base.setUp("Verifying that the user accessed the correct homepage...", "VerifyHomepage");
-        System.out.println("Checking that the user reach the correct landing page...");
+        base.setUp("Verifying that the user accessed the correct landing page...", "VerifyHomepage");
+        System.out.println("Checking that the user reached the correct landing page...");
+        outputToExcel("2", "1", "Navigate to the landing page", "Reached the correct landing page", "Failed");
         AssertJUnit.assertTrue("Wrong homepage...", driver.getTitle().equals(DataItems.homepageTitle));
+        outputToExcel("2", "1", "Navigate to the landing page", "Reached the correct landing page", "Passed");
     }
 
     @Test
@@ -30,9 +31,13 @@ public class LoginTest extends DriverBase {
         base.setUp("Logging in with invalid credentials...", "VerifyInvalidLogin", DataItems.invalidUsername, DataItems.invalidPassword);
         LoginPage loginPage = new LoginPage(driver);
         System.out.println("Checking that the user is on the login page...");
+        outputToExcel("3", "2", "Navigate to the login page", "Reached the correct login page", "Failed");
         AssertJUnit.assertTrue("Wrong page...", driver.getTitle().equals(DataItems.loginTitle));
+        outputToExcel("3", "2", "Navigate to the login page", "Reached the correct login page", "Passed");
         System.out.println("Checking that the error message is received...");
+        outputToExcel("4", "3", "Verify the correct error message", "Error message is correct", "Failed");
         AssertJUnit.assertTrue("Wrong page...", loginPage.errorMessage.getText().equals(DataItems.invalidLogin));
+        outputToExcel("4", "3", "Verify the correct error message", "Error message is correct", "Passed");
     }
 
     @Test
@@ -43,5 +48,8 @@ public class LoginTest extends DriverBase {
 
         Carrefour_Base base = new Carrefour_Base(driver);
         base.setUp("Logging in with valid credentials...", "LoginWithValid", DataItems.validUsername, DataItems.validPassword);
+        outputToExcel("5", "4", "Navigate to the homepage after logging in", "Reached the correct homepage", "Failed");
+        AssertJUnit.assertTrue("Wrong homepage...", driver.getTitle().equals(DataItems.homepageTitle));
+        outputToExcel("5", "4", "Navigate to the homepage after logging in", "Reached the correct homepage", "Passed");
     }
 }
