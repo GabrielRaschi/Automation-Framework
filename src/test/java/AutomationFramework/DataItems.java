bb0848afbb276f122d85fa3b70ca80ee8fbe5670
GabrielRaschi <gabrielraschi@gmail.com>
2017-03-10 23:42:13 +0200

    error commit

--- src/test/java/AutomationFramework/DataItems.java
+++ src/test/java/AutomationFramework/DataItems.java
@@ -2,81 +2,9 @@ package AutomationFramework;
 
 public class DataItems {
 
-    public static String targetURL =" https://prod.ecarrefour.ro/";
-    public static String validUsername = "test01@de.de";
-    public static String changedUsername = "test02@de.de";
-    public static String validPassword = "Tremend10ani";
-    public static String changedPassword = "Tremend11ani";
-
-
-    // -------------- Notification messages
-    public static String wrongLoginMsg = "ceva nu a fost introdus bine..mai incearca o data";
-    public static String correctLoginMsg = "bine ai venit!";
-    public static String correctLogoutMsg = "ai iesit din cont";
-    public static String userUpdateMsg = "Utilizator actualizat";
-    public static String addNewShippingAddressMsg = "ai salvat o noua adresa";
-    public static String shippingAddressUpdateMsg = "adresa a fost actualizata";
-    public static String deleteShippingAddressMsg = "adresa a fost stearsa";
-    public static String addNewInvoiceMsg = "datele de facturare au fost salvate";
-    public static String updateInvoiceMsg = "";
-    public static String deleteInvoiceMsg = "datele de facturare au fost sterse";
-
-    // -------------- Type of notification
-    public static String login = "Login";
-    public static String logout = "Logout";
-    public static String warning = "Warning";
-    public static String addProfile = "ProfileAdd";
-    public static String updateProfile = "ProfileUp";
-    public static String addShppingAddress = "ShippingAdd";
-    public static String updateShppingAddress = "ShippingUp";
-    public static String deleteShippingAddress = "ShippingDel";
-    public static String addInvoice = "InvoiceAdd";
-    public static String updateInvoice = "InvoiceUp";
-    public static String deleteInvoice = "InvoiceDel";
-
-
-    // -------------- Field Labels
-    // --- Login
-    public static String loginFormTabAndTitle[] = {"LoginFormTab ","LoginFormTitle", "intru in cont"};
-    public static String loginEmail[] = {"LoginEmail", "e-mail"};
-    public static String loginPassword[] = {"LoginPassword", "parola"};
-    // --- New Account
-    public static String newAccFormTabAndTitle[] = {"NewAccFormTab", "NewAccFormTitle", "imi fac cont"};
-    public static String firstName[] = {"FirstName", "prenume"};
-    public static String lastName[] = {"LastName", "nume"};
-    public static String newAccEmail[] = {"NewAccEmail", "e-mail"};
-    public static String phoneNumber[] = {"PhoneNumber", "telefon"};
-    public static String newAccPassword[] = {"NewAccPassword", "parola"};
-    public static String newAccConfirmPassword[] = {"NewAccConfirmPassword", "confirm parola"};
-
-
-    // -------------- Empty field, messages after save
-    public static String blankFieldMsgForProfile = "Această valoare nu ar trebui să fie goală.";
-    public static String blankFieldMsgForShipping = "Aceasta valoare trebuie completata";
-    public static String emptyZip = "Cod postal invalid";
-
-
-    // -------------- MyProfileData
-    public static String myProfileData01[] = {"testNume01", "testPrenume01", "test01@de.de", "0754317885"};
-    public static String myProfileData02[] = {"testNume02", "testPrenume02", "test02@de.de", "0754317886"};
-
-    // -------------- ShippingAddressData
-    public static String shippingAddressData01[] = {"testAddressName01", "testStreetName01", "testStreetNo01", "testBuilding01", "testBuildingDoor01", "testFloor01", "testApartment01", "testIntercom01", "060541", "testDistrict01", "Bucuresti Sector 6"};
-    public static String shippingAddressData02[] = {"testAddressName02", "testStreetName02", "testStreetNo02", "testBuilding02", "testBuildingDoor02", "testFloor02", "testApartment02", "testIntercom02", "060543", "testDistrict02", "Bucuresti Sector 6"};
-    public static String userDataShippingAdress01[] = {"testFirstname01", "testLastName01", "testPhone01"};
-    public static String userDataShippingAdress02[] = {"testFirstname02", "testLastName02", "testPhone02"};
-
-    // -------------- InvoiceData
-    public static String invoiceDataForIndividual01[] = {"testInvoiceName01", "testAddress01", "testPerson01"};
-    public static String invoiceDataForIndividual02[] = {"testInvoiceName02", "testAddress02", "testPerson02"};
-    public static String invoiceDataForLegalEntity01[] = {"testInvoiceName01", "testFirmName01", "testFiscalCode01", "testFirmAddress01", "testPerson01"};
-    public static String invoiceDataForLegalEntity02[] = {"testInvoiceName02", "testFirmName02", "testFiscalCode02", "testFirmAddress02", "testPerson02"};
-
-
-    // -------------- Get Attribute
-    public static String placeholder = "placeholder";
-    public static String value = "value";
-
+    public static String targetURL = "https://github.com/GabrielRaschi/Automation-Framework";
+    public static String validUsername = "";
+    public static String validPassword = "";
 
     // -------------- Waiting Time
     public static int shortWait = 10;
--- src/test/java/PageObjects/MainPage.java
+++ src/test/java/PageObjects/MainPage.java
@@ -1,14 +1,11 @@
 package PageObjects;
-import AutomationFramework.DataItems;
-import AutomationFramework.Wait;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+
+import AutomationFramework.CommonTask;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
+
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import static AutomationFramework.Wait.clickable;
 
 public class MainPage {
 
@@ -20,100 +17,23 @@ public class MainPage {
         PageFactory.initElements(passedDriver, this);
     }
 
-    @FindBy(css = "#login-dropdown-toggle > span > span")
-    public WebElement myAccountLocator;
+    //Set locators for bellow elements
 
-    @FindBy(css = "#micro-cart-dropdown")
-    public WebElement cartLocator;
+    @FindBy(id = "")
+    public WebElement usernameField;
 
-    @FindBy(css = "#contact-dropdown > span")
-    public WebElement contactLocator;
+    @FindBy(id = "")
+    public WebElement passwordField;
 
-    @FindBy(css = "div > div > div > div.modal-header.ng-scope > button > span")
-    public WebElement closeButton;
+    @FindBy(id = "")
+    public WebElement loginButton;
 
-
-    // --------- Notifications
-
-    @FindBy(css = "body > div.ui-notification.ng-scope.warning > div")
-    public WebElement notificationWarning;
-
-    @FindBy(css = "body > div.ui-notification.ng-scope.success.killed > div")
-    public WebElement notificationLogin;
-
-    @FindBy(css = "body > div.ui-notification.ng-scope.info > div")
-    public WebElement noticationLogout;
-
-    @FindBy(css = "body > div.ui-notification.ng-scope.success > div")
-    public WebElement notificationUserUpdate;
-
-    @FindBy(css = "body > div.ui-notification.ng-scope.info")
-    public WebElement notificationShippingAddNew;
-
-    @FindBy(css = "body > div.ui-notification.ng-scope.info.killed > div")
-    public WebElement notificationShippingUpdate;
-
-    @FindBy(css = "body > div.ui-notification.ng-scope.info.killed")
-    public WebElement notificationShippingDelete;
-
-    @FindBy(css = "body > div.ui-notification.ng-scope.info")
-    public WebElement notificationInvoiceAddNew;
-
-    @FindBy(css = "body > div.ui-notification.ng-scope.info.killed > div")
-    public WebElement notificationInvoiceDelete;
-
-
-
-
-
-    public String notificationText(String typeOfNotif) {
-        MyAccountObj map = new MyAccountObj(driver);
-        String notificationText = "";
-        if (typeOfNotif.equals(DataItems.login)) {
-            myAccountLocator.click();
-            Wait.visible(driver, notificationLogin);
-            Wait.textPresent(driver, notificationLogin, DataItems.correctLoginMsg);
-            notificationText = notificationLogin.getText();
-        } else if (typeOfNotif.equals(DataItems.logout)) {
-            myAccountLocator.click();
-            map.logoutLocator.click();
-            Wait.visible(driver, noticationLogout);
-            Wait.textPresent(driver, noticationLogout, DataItems.correctLogoutMsg);
-            notificationText = noticationLogout.getText();
-        } else if (typeOfNotif.equals(DataItems.warning)) {
-            Wait.visible(driver, notificationWarning);
-            Wait.textPresent(driver, notificationWarning, DataItems.wrongLoginMsg);
-            notificationText = notificationWarning.getText();
-            driver.navigate().refresh();
-            Wait.visible(driver, myAccountLocator);
-        } else if (typeOfNotif.equals(DataItems.updateProfile)) {
-            Wait.visible(driver, notificationUserUpdate);
-            Wait.textPresent(driver, notificationUserUpdate, DataItems.userUpdateMsg);
-            notificationText = notificationUserUpdate.getText();
-        }else if (typeOfNotif.equals(DataItems.addShppingAddress)) {
-            Wait.visible(driver, notificationShippingAddNew);
-            Wait.textPresent(driver, notificationShippingAddNew, DataItems.addNewShippingAddressMsg);
-            notificationText = notificationShippingAddNew.getText();
-        } else if (typeOfNotif.equals(DataItems.updateShppingAddress)) {
-            Wait.visible(driver, notificationShippingUpdate);
-            Wait.textPresent(driver, notificationShippingUpdate, DataItems.shippingAddressUpdateMsg);
-            notificationText = notificationShippingUpdate.getText();
-        } else if (typeOfNotif.equals(DataItems.deleteShippingAddress)) {
-            Wait.visible(driver, notificationShippingDelete);
-            Wait.textPresent(driver, notificationShippingDelete, DataItems.deleteShippingAddressMsg);
-            notificationText = notificationShippingDelete.getText();
-        } else if (typeOfNotif.equals(DataItems.addInvoice)) {
-            Wait.visible(driver, notificationInvoiceAddNew);
-            Wait.textPresent(driver, notificationInvoiceAddNew, DataItems.addNewInvoiceMsg);
-            notificationText = notificationInvoiceAddNew.getText();
-        }else if (typeOfNotif.equals(DataItems.deleteInvoice)) {
-            Wait.visible(driver, notificationInvoiceDelete);
-            Wait.textPresent(driver, notificationInvoiceDelete, DataItems.deleteInvoiceMsg);
-            notificationText = notificationInvoiceDelete.getText();
-        }
-
-        return notificationText;
+    public void loginWithUsernameAndPassword(String username, String password){
+        CommonTask.setInputField(driver, usernameField, username);
+        CommonTask.setInputField(driver, passwordField, password);
+        loginButton.click();
     }
+
 
 
 }
